b358d4d58969e117afe4fca93289680478c30699
James Shields <james@lostcarpark.com>
2022-09-01 19:53:41 +0100

    Work in progress on participant links.

---
+++ webpages/ReportMenuInclude.php
@@ -0,0 +1,20 @@
+<li><a href='staffReportsInCategory.php?reportcategory=Administration+Reports'>Administration Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Anime+Reports'>Anime Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Conflict+Reports'>Conflict Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Events+Reports'>Events Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Fast+Track+Reports'>Fast Track Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Film+Reports'>Film Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=GOH+Reports'>GOH Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Gaming+Reports'>Gaming Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Grid+Reports'>Grid Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Hotel+Reports'>Hotel Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Participant+Info+Reports'>Participant Info Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=PlanZ+Administration+Reports'>PlanZ Administration Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Program+Ops+Reports'>Program Ops Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Programming+Reports'>Programming Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Publication+Reports'>Publication Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Registration+Reports'>Registration Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Reports+downloadable+as+CSVs'>Reports downloadable as CSVs</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Security+Reports'>Security Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=Tech+Reports'>Tech Reports</a></li>
+<li><a href='staffReportsInCategory.php?reportcategory=WisCon+Custom+Reports'>WisCon Custom Reports</a></li>
